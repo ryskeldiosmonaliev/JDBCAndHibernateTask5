ccc54f6ad131da7e9469b278378e8085b69b10af
Рыскелди <ryskeldi.osmonaliev@mail.ru>
2022-10-04 20:35:40 +0300

    commit hibernetno

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -23,30 +23,5 @@ public class Main {
 //        //  userService.removeUserById(1);
 
 
-        UserService us = new UserServiceImpl();
-
-        us.createUsersTable();
-
-        User user1 = new User("Nurperi", "Arzymatova", (byte) 19);
-        User user2 = new User("Atabek", "Issakunov", (byte) 20);
-        User user3 = new User("Muktarbek", "Kubanov", (byte) 20);
-        User user4 = new User("Aisezim", "Orozbaeva", (byte) 19);
-
-        us.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
-        us.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
-        us.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
-        us.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
-
-        List<User> list = us.getAllUsers();
-        System.out.print(list + "\n");
-
-        us.removeUserById(1);
-        us.removeUserById(2);
-
-        us.dropUsersTable();
-
-        us.cleanUsersTable();
-
-        Util.shutDown();
     }
 }
